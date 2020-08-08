056fbd6e8b83b6d9dac168fd0e9fbe219d9fe07a
ysoque3 <ysoque@gmail.com>
2020-08-08 21:22:35 +0800

    First code change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Making Your Future a Success';
+  title = 'Making Your Future a Success with iPrutec';
 }
