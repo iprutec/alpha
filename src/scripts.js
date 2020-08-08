fa31168097d43e84218a78ded85c45aba7dd34d5
ysoque3 <ysoque@gmail.com>
2020-08-09 05:07:57 +0800

    Splash Header Build
    Intro Heading/Paragraph/Logo/Scroll Down added

--- src/scripts.js
+++ src/scripts.js
@@ -22,7 +22,7 @@
                     {
                         scrollTop: target.offset().top - 72,
                     },
-                    1000,
+                    3000,
                     "easeInOutExpo"
                 );
                 return false;
